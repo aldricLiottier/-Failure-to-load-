15d602a9b06480bd6f4dd9812bb8fc4eb3abd2fc
Aldric <aldric.liottier@epitech.eu>
2023-03-10 16:05:39 +0100

    0.1

--- cicd.py
+++ cicd.py
@@ -2,7 +2,6 @@ import subprocess
 import threading
 import time
 
-
 def output_reader(proc):
     for line in iter(proc.stdout.readline, b''):
         if ("Failed to compile." in line.decode('utf-8')):
@@ -26,9 +25,15 @@ def main():
             proc.wait(timeout=0.2)
             print('== subprocess exited with rc =', proc.returncode)
             if proc.returncode == 0:
+                f = open("VERSION.TMP", "r")
+                tmp =  float(f.read())
+                f.close()
                 subprocess.run("git add *", shell=True, check=False)
-                subprocess.run("git commit -m 'temp'", shell=True, check=False)
+                subprocess.run("git commit -m '" + str(tmp) + "'", shell=True, check=False)
                 subprocess.run("git push", shell=True, check=False)
+                f = open("VERSION.TMP", "w")
+                f.write(str(tmp + 0.1))
+                f.close()
         except subprocess.TimeoutExpired:
             print('subprocess did not terminate in time')
     t.join()
